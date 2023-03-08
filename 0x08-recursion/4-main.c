6f6ebf86482fb35b100c5220fd6f07d316ae7ed7
Neylwe <keneilwe.mosala@ymail.com>
2023-03-08 03:17:49 -0800

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-main.c
+++ 0x08-recursion/4-main.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("%d\n", r);
 	r = _pow_recursion(1024, 0);
 	printf("%d\n", r);
-	r = _pow_recursion(2, 16)
+	r = _pow_recursion(2, 16);
 	printf("%d\n", r);
 	r = _pow_recursion(5, 2);
 	printf("%d\n", r);
