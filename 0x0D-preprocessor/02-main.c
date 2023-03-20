efce28a03fe61c5ff5c88a49deeee3739fb51ec7
Neylwe <keneilwe.mosala@ymail.com>
2023-03-20 01:04:36 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - writes a function-like macro ABS (x) that computes the absolute
+ * value of a number of x
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
