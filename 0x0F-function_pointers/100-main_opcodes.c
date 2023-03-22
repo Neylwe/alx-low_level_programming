fae7153f7ccc4ba7c9b92689912e08162cc5014b
Neylwe <keneilwe.mosala@ymail.com>
2023-03-22 12:28:01 -0700

    program prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,44 +2,36 @@
 #include <stdio.h>
 
 /**
- * print_opcodes - program that print the opcodes of its own main function.
- * @s: Address of the main function.
- * @x: Number of bytes to print.
- **/
-void print_opcodes(char *s, int x)
-{
-	int a;
-
-	for (; a < x; a++)
-	{
-		printf("%.2hhx", s[a]);
-		if (a < x - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
-
-/**
- * main - program that print the opcodes of its own main function.
+ * main - program that prints the opcodes of
+ * its own main function
  * @argc: argument counter
- * @argv: Argument values
- * Return: O (success).
+ * @argv: argument values
+ * Return: Always 0 (success)
  */
 int main(int argc, char **argv)
 {
-	int b;
+	int count;
 
+	count = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	b = atoi(argv[1]);
-	if (b < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	print_opcodes((char *)&main, b);
+	while (count < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + count));
+		count++;
+		if (atoi(argv[1]) > count)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 	return (0);
 }
